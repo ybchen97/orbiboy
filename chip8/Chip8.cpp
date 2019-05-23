12c5adb0ac4224e7fc057f0682f4ba3c046cbd61
yuanbo <yuanbo.chen97@gmail.com>
2019-05-23 20:57:19 +0800

    implemented initialize, runEmulationCycle and fetch

--- chip8/Chip8.cpp
+++ chip8/Chip8.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 #include <string>
 
 using namespace std;
@@ -20,17 +20,9 @@ class Chip8 {
 
     public:
         // Functions
-        void initialize() {
-            // Initialize 
-        }
-
-        void loadGame(string) {
-
-        }
-
-        void runEmulationCycle() {
-
-        }
+        void initialize();
+        void loadGame(string);
+        void runEmulationCycle(); 
 
     private:
         // Attributes
@@ -38,23 +30,64 @@ class Chip8 {
         BYTE dataRegisters[16];
         WORD I;
         WORD programCounter;
+        stack<WORD> gameStack;
 
         // Functions
-        BYTE fetch(WORD) {
-
-        }
-
-        int decode(BYTE) {
-
-        }
-
-        void execute(int) {
-
-        }
-
-    BYTE gameMemory[4096];
-    BYTE dataRegisters[16];
-    WORD I;
-    WORD pc;
+        WORD fetch(WORD);
+        int decode(WORD);
+        void execute(int);
 
 };
+
+void Chip8::initialize() {
+    
+    // Clears memory and registers, sets pc to 0x200
+    memset(gameMemory, 0, sizeof(gameMemory));
+    memset(dataRegisters, 0, sizeof(dataRegisters));
+    I = 0;
+    programCounter = 0x200;
+
+}
+
+void Chip8::loadGame(string path) {
+
+}
+
+void Chip8::runEmulationCycle() {
+
+    // running emulation cycle
+    while (true) {
+        
+        // fetch, decode and execute cycle
+        WORD opcode = this->fetch(this->programCounter);
+        int functionNum = this->decode(opcode);
+        this->execute(functionNum);
+
+    }
+
+}
+
+WORD Chip8::fetch(WORD pc) {
+
+    /*
+    Fetches two successive bytes from the memory, since opcodes are 16 bit 
+    (i.e. 2 bytes long). 
+    1. Get data in memory where PC is pointing at, shift it left 8 places, call 
+    it MSB
+    2. Get data at PC+1, do bitwise or with MSB to get full opcode
+    */
+    
+    WORD opcode = (gameMemory[this->programCounter] << 8) | gameMemory[this->programCounter];
+
+    // increment program counter i.e. PC += 
+    this->programCounter += 2;
+
+}
+
+int Chip8::decode(WORD opcode) {
+
+}
+
+void Chip8::execute(int num) {
+
+}
