3e7e4819e1380e26b5cae461757cbff2054ab6b8
yuanbo <yuanbo.chen97@gmail.com>
2019-05-29 22:46:19 +0800

    changed game to tetris

--- chip8/Main.cpp
+++ chip8/Main.cpp
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
     uint32_t pixels[2048];
 
     // Attempt to load ROM
-    if (!chip8.loadGame("INVADERS")) {
+    if (!chip8.loadGame("TETRIS")) {
         printf("Some problem occured while loading!\n");
         return 2;
     }
